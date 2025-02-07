e097e758bb8295db00cd2f88236820a46f5d8556
PNS212624\Hansol <yrlee00@hansol.com>
2025-02-07 16:31:46 +0900

    feat: app.js 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import App from './App.js'
+import Header from "./components/Header";
+import RegionList from "./components/RegionList";
+import CityList from "./components/CityList";
+import CityDetail from "./components/CityDetail";
 
-const $app = document.getElementById('app');
+export default function App() {
+    this.state = {
+        startIdx: 0,
+        sortBy: '',
+        searchWord: '',
+        region: '',
+        cities: ''
+    };
 
-new App($app);
+    const header = new Header();
+    const regionList = new RegionList();
+    const cityList = new CityList({$app, initialState : this.state.cities});
+    const cityDetail = new CityDetail();
+
+    this.setState = (newState) => {
+        this.state = newState;
+        cityList.setState(this.state.cities)
+    };
+
+    const init = () => {};
+
+    init();
+}
