7e5b1a353d33f4164e0431afdf1cfe26b3e07347
PNS212624\Hansol <yrlee00@hansol.com>
2025-02-10 17:04:46 +0900

    feat: regionList.js 구현

--- src/components/RegionList.js
+++ src/components/RegionList.js
@@ -1,10 +1,47 @@
-export default function RegionList() {
+export default function RegionList({ $app, initialState, handleRegion }) {
+    this.state = initialState;
     this.$target = document.createElement('div');
-    this.$target.className = 'header';
+    this.$target.className = 'region-list';
 
-    this.template = () => {};
+    this.handleRegion = handleRegion;
+    $app.appendChild(this.$target);
 
-    this.render = () => {};
+    this.template = () => {
+        const regionList = [
+            '🚀 All',
+            '🌏 Asia',
+            '🕌 Middle-East',
+            '🇪🇺 Europe',
+            '💃 Latin-America',
+            '🐘 Africa',
+            '🏈 North-America',
+            '🏄 Oceania',
+        ];
+        let temp = ``;
+        regionList.forEach((elm) => {
+            let regionId = elm.split(' ')[1];
+            temp += `<div id=${regionId}>${elm}</div>`;
+        });
+        return temp;
+    };
+
+    this.render = () => {
+        this.$target.innerHTML = this.template();
+        let $currentRegion;
+        if (this.state) {
+            $currentRegion = document.getElementById(this.state);
+            $currentRegion && ($currentRegion.className = 'clicked');
+        } else {
+            document.getElementById('All').className = 'clicked';
+        }
+
+        const $regionList = this.$target.querySelectorAll('div');
+        $regionList.forEach((elm) => {
+            elm.addEventListener('click', () => {
+                this.handleRegion(elm.id);
+            });
+        });
+    };
 
     this.setState = (newState) => {
         this.state = newState;
